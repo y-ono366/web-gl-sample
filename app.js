3d09a6717b915c96d7cd0693812ab99a72cbe206
Yusuke Ono <y.ono.varial@gmail.com>
2018-11-10 23:56:05 +0900

    fix syntax

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -19,12 +18,12 @@ app.use(express.static(path.join(__dirname, 'src/assets')));
 app.use('/', index);
 
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
